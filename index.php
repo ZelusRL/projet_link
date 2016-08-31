f9058d0dd9b9d1b6a820ccaecf5df0643ddc9fad
ZelusRL <zelus.guillaume@gmail.com>
2016-08-31 16:35:41 +0200

    header + footer + home + login

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$db = mysqli_connect("localhost", "root", "troiswa", "projet_link");
+$error = "";
+$error404 = "";
+$page = "home";
+
+$access = ["home", "login", "register", "articles"];
+$accessIn = ["home", "login", "register","articles"];
+if(isset($_SESSION["in"]))
+{
+	if(isset($_GET["page"]) && in_array($_GET["page"], $accessIn))
+	{
+		$page = $_GET["page"];
+	}	
+}
+else
+{
+	if(isset($_GET["page"]) && in_array($_GET["page"], $access))
+	{
+		$page = $_GET["page"];
+	}	
+}
+
+$traitementList = ["creation", "login", "modif", "delete", "register","single"];
+if(in_array($page, $traitementList))
+{
+	require("apps/traitement_".$page.".php");
+}
+require("apps/skel.php");
+?>
